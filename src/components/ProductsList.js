8c066f7256f3c8c4c1cb730016848e26ba24a504
Mark <markwp02@gmail.com>
2023-10-30 12:41:41 +1100

    Refactor - remove commented className
    - Remove commented out className used for Tailwind css

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -17,7 +17,7 @@ function ProductsList() {
             return <ShowProduct key={product.productId}>{product}</ShowProduct>
         });
     }
-//grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+
     return ( 
         <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {content}

3e74bfcb026bdffa3e0a1f139871518e2cf9d97e
Mark <markwp02@gmail.com>
2023-11-18 17:25:40 +1100

    Create SignupPage
    - Created SignupPage

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,63 @@
+import { LOGIN_PAGE_PATH } from "../constants/PathConstants";
+import useNavigation from "../hooks/use-navigation";
+
+function SignupPage() {
+    const { navigate } = useNavigation();
+
+    /**
+     * Method to handle navigation
+     * Meta key (Mac) and ctrl key (Windows) will use the href to navigate
+     * to a new tab.
+     * Prevent default to stop the app from refetching data from the server.
+     * @param {*} event 
+     * @returns 
+     */
+    const handleLoginClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
+        event.preventDefault();
+        navigate(LOGIN_PAGE_PATH);
+    };
+    return (
+        <section>
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Create your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your first name</label>
+                                <input type="text" name="first-name" id="first-name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your last name</label>
+                                <input type="text" name="last-name" id="last-name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            </div>
+                            <button type="submit" className="button is-primary w-full">Create account</button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Already have an account? <a href={LOGIN_PAGE_PATH} className="font-medium text-primary-600 hover:underline dark:text-primary-500" onClick={handleLoginClick}>Login here</a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+      </section>
+    );
+}
+
+export default SignupPage;
